835151c1523d5f420c22d941d6b95d6136f0cf7a
nunomi0 <ykys1@naver.com>
2024-03-20 16:22:42 +0900

    feat : set theme resource

---
+++ lib/ui/theme/color.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color highlightDarkest = Color.fromRGBO(26, 147, 111, 1);
+  static const Color highlightDark = Color.fromRGBO(26, 147, 89, 0.77);
+  static const Color highlightMedium = Color.fromRGBO(136, 212, 152, 1);
+  static const Color highlightLight = Color.fromRGBO(215, 233, 209, 0.67);
+  static const Color highlightLightest = Color.fromRGBO(231, 241, 231, 0.19);
+
+
+  static const Color neutralLightDarkest = Color.fromRGBO(197, 198, 204, 1);
+  static const Color neutralLightDark = Color.fromRGBO(212, 214, 221, 1);
+  static const Color neutralLightMedium = Color.fromRGBO(232, 233, 241, 1);
+  static const Color neutralLightLight = Color.fromRGBO(248, 249, 254, 1);
+  static const Color neutralLightLightest = Color.fromRGBO(255, 255, 255, 1);
+
+  static const Color neutralDarkDarkest = Color.fromRGBO(31, 32, 36, 1);
+  static const Color neutralDarkDark = Color.fromRGBO(47, 48, 54, 1);
+  static const Color neutralDarkMedium = Color.fromRGBO(73, 74, 80, 1);
+  static const Color neutralDarkLight = Color.fromRGBO(113, 114, 122, 1);
+  static const Color neutralDarkLightest = Color.fromRGBO(143, 144, 152, 1);
+
+
+  static const Color supportSuccessDark = Color.fromRGBO(41, 130, 103, 1);
+  static const Color supportSuccessMedium = Color.fromRGBO(58, 192, 160, 1);
+  static const Color supportSuccessLight = Color.fromRGBO(231, 244, 232, 1);
+
+  static const Color supportWarningDark = Color.fromRGBO(232, 99, 57, 1);
+  static const Color supportWarningMedium = Color.fromRGBO(255, 179, 124, 1);
+  static const Color supportWarningLight = Color.fromRGBO(255, 244, 228, 1);
+
+  static const Color supportErrorDark = Color.fromRGBO(237, 50, 65, 1);
+  static const Color supportErrorMedium = Color.fromRGBO(255, 97, 109, 1);
+  static const Color supportErrorLight = Color.fromRGBO(255, 226, 229, 1);
+}
---
+++ lib/ui/theme/text_styles.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle headingH1 = TextStyle(
+    fontSize: 24.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headingH2 = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headingH3 = TextStyle(
+    fontSize: 16.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headingH4 = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle headingH5 = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.bold,
+  );
+
+
+  static const TextStyle bodyXL = TextStyle(
+    fontSize: 18.0,
+  );
+
+  static const TextStyle bodyL = TextStyle(
+    fontSize: 16.0,
+  );
+
+  static const TextStyle bodyM = TextStyle(
+    fontSize: 14.0,
+  );
+
+  static const TextStyle bodyS = TextStyle(
+    fontSize: 12.0,
+  );
+
+  static const TextStyle bodyXS = TextStyle(
+    fontSize: 10.0,
+  );
+
+
+  static const TextStyle actionL = TextStyle(
+    fontSize: 14.0,
+  );
+
+  static const TextStyle actionM = TextStyle(
+    fontSize: 12.0,
+  );
+
+  static const TextStyle actionS = TextStyle(
+    fontSize: 10.0,
+  );
+
+  static const TextStyle captionM = TextStyle(
+    fontSize: 10.0,
+  );
+}
